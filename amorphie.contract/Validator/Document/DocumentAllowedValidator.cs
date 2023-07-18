63c168bcd3c658fdb0e16937b238061917df1cd7
Fatih <my.fatih.92@hotmail.com>
2023-07-18 11:15:47 +0300

    ok.
    ok.

---
+++ amorphie.contract/Validator/Document/DocumentAllowedValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+using amorphie.contract.core.Entity.Document;
+
+public sealed class DocumentAllowedValidator : AbstractValidator<DocumentAllowed>
+    {
+        public DocumentAllowedValidator()
+        {
+            // RuleFor(x => x.Name).NotNull();
+            // RuleFor(x => x.Name).MinimumLength(10);
+        }
+    }
