42932d90f39da697de8677a9fe8acbb9dc36b417
Fatih <my.fatih.92@hotmail.com>
2023-09-05 15:34:07 +0300

    ok

---
+++ amorphie.contract/Validator/Document/DocumentTypes/DocumentOnlineSingValidator.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+using amorphie.contract.core.Entity.Document.DocumentTypes;
+
+public sealed class DocumentOnlineSingValidator : AbstractValidator<DocumentOnlineSing>
+    {
+        public DocumentOnlineSingValidator()
+        {
+            // RuleFor(x => x.Name).NotNull();
+            // RuleFor(x => x.Name).MinimumLength(10);
+        }
+    }
