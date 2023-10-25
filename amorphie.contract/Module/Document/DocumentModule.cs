1d369f2343e15d2f0bfa7a1e621e179b2a111330
Fatih <my.fatih.92@hotmail.com>
2023-10-25 20:49:57 +0300

    ok
    ok

--- amorphie.contract/Module/Document/DocumentModule.cs
+++ amorphie.contract/Module/Document/DocumentModule.cs
@@ -100,10 +100,77 @@ public class DocumentModule
 
         return Results.NoContent();
     }
+     async ValueTask<IResult> getAllDocumentAll(
+           [FromServices] ProjectDbContext context ,
+           CancellationToken cancellationToken
+      )
+    {
+        try
+        {
+
+
+            var query = context!.Document;
+
+            var securityQuestions = await query.ToListAsync(cancellationToken);
+
+            if (securityQuestions.Any())
+            {
+
+                // var response = securityQuestions.Select(x => ObjectMapper.Mapper.Map<RootDocumentModel>(x));
+                var response = securityQuestions.Select(x =>
+                 new RootDocumentModel
+                 {
+                     Id = x.Id.ToString(),
+                     DocumentDefinitionId = x.DocumentDefinitionId.ToString(),
+                     StatuCode = x.Status.Code,
+                     CreatedAt = x.CreatedAt,
+                     DocumentDefinition = new DocumentDefinitionModel
+                     {
+                         Code = x.DocumentDefinition.Code,
+                         MultilanguageText = x.DocumentDefinition.DocumentDefinitionLanguageDetails!
+                                .Select(a => new MultilanguageText
+                                {
+                                    Label = a.MultiLanguage.Name,
+                                    Language = a.MultiLanguage.LanguageType.Code
+                                }).ToList(),
+                         DocumentOperations = new DocumentOperationsModel
+                         {
+                             DocumentManuelControl = x.DocumentDefinition.DocumentOperations!.DocumentManuelControl,
+                             DocumentOperationsTagsDetail = x.DocumentDefinition.DocumentOperations.DocumentOperationsTagsDetail!.Select(x => new TagModel
+                             {
+                                 Contact = x.Tags.Contact,
+                                 Code = x.Tags.Code
+                             }).ToList()
+                         }
+                     },
+                     DocumentContent = new DocumentContentModel
+                     {
+                         ContentData = x.DocumentContent.ContentData,
+                         KiloBytesSize = x.DocumentContent.KiloBytesSize,
+                         ContentType = x.DocumentContent.ContentType,
+                         ContentTransferEncoding = x.DocumentContent.ContentTransferEncoding,
+                         Name = x.DocumentContent.Name,
+                         Id = x.DocumentContent.Id.ToString()
+
+                     }
+                 }).ToList();
+
+
+                return Results.Ok(response);
+            }
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+
+        return Results.NoContent();
+    }
     public override void AddRoutes(RouteGroupBuilder routeGroupBuilder)
     {
         base.AddRoutes(routeGroupBuilder);
         routeGroupBuilder.MapGet("search", getAllDocumentFullTextSearch);
+        routeGroupBuilder.MapGet("getAll", getAllDocumentAll);
     }
 
 }
