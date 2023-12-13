da945157652c031e7ff862c9886afb6911195c1d
UmutCanoglu <umuttcanoglu@gmail.com>
2023-12-13 13:26:45 +0300

    Get template changes.

---
+++ amorphie.contract/Models/Proxy/TemplateEngineDefinitionResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace amorphie.contract.Models.Proxy
+{
+	public class TemplateEngineDefinitionResponseModel
+	{
+		public TemplateEngineDefinitionResponseModel()
+		{
+		}
+
+		public string Name { get; set; }
+		public List<string> SemanticVersions { get; set; }
+	}
+}
+
