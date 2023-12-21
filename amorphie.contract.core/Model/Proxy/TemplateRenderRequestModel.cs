0a2924a8ce55eae323c59e8f499e1664078d7f0c
Fatih <my.fatih.92@hotmail.com>
2023-12-21 15:13:28 +0300

    ok
    ok

--- amorphie.contract.core/Model/Proxy/TemplateRenderRequestModel.cs
+++ amorphie.contract.core/Model/Proxy/TemplateRenderRequestModel.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Text.Json.Serialization;
 using Newtonsoft.Json;
-
-namespace amorphie.contract.Models.Proxy
+namespace amorphie.contract.core.Model.Proxy
 {
     public class TemplateRenderRequestModel
     {
@@ -11,31 +10,35 @@ namespace amorphie.contract.Models.Proxy
         }
 
         public string Name { get; set; }
-        [JsonProperty("render-id")]
+        [Newtonsoft.Json.JsonProperty("render-id")]
         [JsonPropertyName("render-id")]
         public Guid RenderId { get; set; }
-        [JsonProperty("render-data")]
+        [Newtonsoft.Json.JsonProperty("render-data")]
         [JsonPropertyName("render-data")]
         public object RenderData { get; set; }
-        [JsonProperty("render-data-for-log")]
+        [Newtonsoft.Json.JsonProperty("render-data-for-log")]
         [JsonPropertyName("render-data-for-log")]
         public object RenderDataForLog { get; set; }
-        [JsonProperty("semantic-version")]
+        [Newtonsoft.Json.JsonProperty("semantic-version")]
         [JsonPropertyName("semantic-version")]
         public string SemanticVersion { get; set; }
-        [JsonProperty("process-name")]
+        [Newtonsoft.Json.JsonProperty("process-name")]
         [JsonPropertyName("process-name")]
         public string ProcessName { get; set; }
-        [JsonProperty("item-id")]
+        [Newtonsoft.Json.JsonProperty("item-id")]
         [JsonPropertyName("item-id")]
         public string ItemId { get; set; }
         public string Action { get; set; }
         public string Identity { get; set; }
         public string Customer { get; set; }
-        [JsonProperty("children-name")]
+        [Newtonsoft.Json.JsonProperty("children-name")]
         [JsonPropertyName("children-name")]
         public string ChildrenName { get; set; }
         public List<object> Children { get; set; }
+    }
+
+    internal class JsonPropertyAttribute : Attribute
+    {
     }
 }
 
