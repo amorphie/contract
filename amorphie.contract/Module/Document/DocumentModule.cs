ae744094d93fbdb8a4845add1b41de3100a4b86b
UmutCanoglu <umuttcanoglu@gmail.com>
2023-12-22 15:29:19 +0300

    Add customer module.

--- amorphie.contract/Module/Document/DocumentModule.cs
+++ amorphie.contract/Module/Document/DocumentModule.cs
@@ -177,24 +177,8 @@ public class DocumentModule
         routeGroupBuilder.MapGet("search", getAllDocumentFullTextSearch);
         routeGroupBuilder.MapGet("getAll", getAllDocumentAll);
         routeGroupBuilder.MapPost("Instance", Instance);
-        routeGroupBuilder.MapGet("CustomerDocument", CustomerDocument);
     }
-    async ValueTask<IResult> CustomerDocument([FromServices] ProjectDbContext context, [FromServices] IMapper mapper,
-                HttpContext httpContext, CancellationToken token, string reference)
-    {
-        var d = await context.Document.Where(x => x.Customer.Reference == reference)
-        .Select(x => new
-        {
-            // x.DocumentDefinitionId,
-            x.DocumentDefinition.Code,
-            x.DocumentDefinition.Semver,
-            status = "valid",
-            x.DocumentContent.MinioObjectName,
-            x.DocumentContent.ContentData,
-            x.Customer.Reference
-        }).ToListAsync(token);
-        return Results.Ok(d);
-    }
+
     async ValueTask<IResult> Instance([FromServices] ProjectDbContext context, [FromServices] IMapper mapper,
                     HttpContext httpContext, CancellationToken token,
                     // IFormFile file,
