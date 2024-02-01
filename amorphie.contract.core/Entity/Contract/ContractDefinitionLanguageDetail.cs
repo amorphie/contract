149a37119132047eb311f351059293c338917165
Enes Can Işık <53035239+themiralay@users.noreply.github.com>
2024-02-01 23:34:49 +0300

    Zebee Interface moved
    zebee interface moved to in services
    ContractDefinitionLanguageDetail Api added for contract layer

--- amorphie.contract.core/Entity/Contract/ContractDefinitionLanguageDetail.cs
+++ amorphie.contract.core/Entity/Contract/ContractDefinitionLanguageDetail.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using amorphie.contract.core.Entity.Base;
-using amorphie.contract.core.Entity.Contract;
 
 namespace amorphie.contract.core.Entity.Contract;
 
