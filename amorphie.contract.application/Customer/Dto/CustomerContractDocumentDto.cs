6c97c98d299e68edd769475f9d3b2c799272d8c7
UmutCanoglu <umuttcanoglu@gmail.com>
2024-02-02 15:00:52 +0300

    Dto fix.

---
+++ amorphie.contract.application/Customer/Dto/CustomerContractDocumentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using amorphie.contract.application.Contract.Dto;
+using amorphie.core.Base;
+
+namespace amorphie.contract.application.Customer.Dto
+{
+	public class CustomerContractDocumentDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public string DocumentStatus { get; set; } = AppConsts.NotValid;
+        public bool Required { get; set; }
+        public bool Render { get; set; }
+        public string Version { get; set; }
+        public string MinioUrl { get; set; }
+        public OnlineSignDto OnlineSign { get; set; }
+        public List<MultilanguageText> MultiLanguageText { get; set; }
+    }
+}
+
---
+++ amorphie.contract.application/Customer/Dto/CustomerContractDocumentGroupDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using amorphie.core.Base;
+
+namespace amorphie.contract.application.Customer.Dto
+{
+	public class CustomerContractDocumentGroupDto
+	{
+		public Guid Id { get; set; }
+		public string Code { get; set; }
+		public string DocumentGroupStatus { get; set; } = AppConsts.NotValid;
+		public ushort AtLeastRequiredDocument { get; set; }
+        public bool Required { get; set; }
+        public List<CustomerContractDocumentDto> CustomerContractGroupDocuments { get; set; }
+        public List<MultilanguageText> MultiLanguageText { get; set; }
+	}
+}
+
