a63d16d917f2411bcaccdddee3db501c2eab2c08
serhatdemir18 <sedemir@burgan.com.tr>
2024-02-08 13:25:47 +0300

    added k6 load test and integration test case

--- tests/k6/get_documents_by_contracts.js
+++ tests/k6/get_documents_by_contracts.js
@@ -1,27 +1,42 @@
+import { check, group, sleep } from 'k6';
 import http from 'k6/http';
-import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 5,
-  maxVUs: 2000,
+  vus: 10,
   stages: [
-    { duration: '2m', target: 1000 }, // 2 dakika boyunca 1000 request atılacak
-    { duration: '1m', target: 2000 }, // Sonraki 1 dakikada 2000 request atılacak
+    { duration: "30s", target: 10 },
+    { duration: "5m", target: 20 },
   ],
-};
+  thresholds: {
+    "http_req_duration": ["avg<500"]
+  }
+}
+
+const BASE_URL = "https://test-amorphie-contract.burgan.com.tr";
+const LanguageCode = ["en-EN", "tr-TR"];
 
 export default function () {
+  group('Contract API testing', function () {
+    group('customer', function () {
 
-  const randomNumber = Math.floor(Math.random() * 50);
-  const randomReference = Math.floor(Math.random() * (100000 - 10000 + 1) + 10000);
+      const randomNumber = Math.floor(Math.random() * 50);
+      const randomReference = Math.floor(Math.random() * (100000 - 10000 + 1) + 10000);
+      const randomLangCode = Math.floor(Math.random() * 2);
 
+      let url = `${BASE_URL}/customer/get-documents-by-contracts?Code=test-hesap-acilis&Page=0&PageSize=${randomNumber}&Reference=${randomReference}`;
 
-  let url = `https://test-amorphie-contract.burgan.com.tr/customer/get-documents-by-contracts?Code=test-hesap-acilis&Page=0&PageSize=${randomNumber}&Reference=${randomReference}`;
+      const requestHeaders = {
+        'User-Agent': 'k6-serhat',
+        'Accept-Language': LanguageCode[randomLangCode],
+      };
 
-  let response = http.get(url);
-  check(response, {
+      let res = http.get(url, {
+        headers: requestHeaders
+      });
+
+      check(res, { "status is 200": (r) => r.status === 200 });
+    });
 
   });
-
-  sleep(Math.random() * 3);
+  sleep(1);
 }
