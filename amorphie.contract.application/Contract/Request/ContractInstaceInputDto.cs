0d2c263004cab777ded56c96f086e9aa5c4b2005
Fatih <my.fatih.92@hotmail.com>
2024-02-09 14:53:30 +0300

    ok
    ok

--- amorphie.contract.application/Contract/Request/ContractInstaceInputDto.cs
+++ amorphie.contract.application/Contract/Request/ContractInstaceInputDto.cs
@@ -6,8 +6,8 @@ namespace amorphie.contract.application.Contract.Request
     {
         public string ContractName { get; set; }
         public string Reference { get; set; }
-        public string? LangCode { get; set; }
-        public EBankEntity? EBankEntity { get; set; }
+        public string LangCode { get; set; }
+        public EBankEntity EBankEntity { get; set; }
         //public string Owner { get; set; }
         //public string CallbackName { get; set; }
         //public Guid ProcessId { get; set; }
