777df53c56e5a1b889d075bfc21a16ef7dabd79b
Enes Can Işık <53035239+themiralay@users.noreply.github.com>
2024-02-19 15:52:05 +0300

    Update StringHelper.cs

--- amorphie.contract.zeebe/Helper/StringHelper.cs
+++ amorphie.contract.zeebe/Helper/StringHelper.cs
@@ -1,23 +1,26 @@
-public static class StringHelper
+namespace amorphie.contract.zeebe.Helper
 {
-    public static int CompareVersions(string version1, string version2)
-        {
-            string[] parts1 = version1.Split('.');
-            string[] parts2 = version2.Split('.');
-            int length = Math.Max(parts1.Length, parts2.Length);
-            for (int i = 0; i < length; i++)
+    public static class StringHelper
+    {
+        public static int CompareVersions(string version1, string version2)
             {
-                int num1 = (i < parts1.Length) ? int.Parse(parts1[i]) : 0;
-                int num2 = (i < parts2.Length) ? int.Parse(parts2[i]) : 0;
-                if (num1 < num2)
+                string[] parts1 = version1.Split('.');
+                string[] parts2 = version2.Split('.');
+                int length = Math.Max(parts1.Length, parts2.Length);
+                for (int i = 0; i < length; i++)
                 {
-                    return -1;
+                    int num1 = (i < parts1.Length) ? int.Parse(parts1[i]) : 0;
+                    int num2 = (i < parts2.Length) ? int.Parse(parts2[i]) : 0;
+                    if (num1 < num2)
+                    {
+                        return -1;
+                    }
+                    else if (num1 > num2)
+                    {
+                        return 1;
+                    }
                 }
-                else if (num1 > num2)
-                {
-                    return 1;
-                }
+                return 0;
             }
-            return 0;
-        }
+    }
 }
