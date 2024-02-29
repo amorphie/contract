1f049611e7ab6dfcc8ef35dc6989400f5f498ad0
Fatih <my.fatih.92@hotmail.com>
2024-02-29 21:50:11 +0300

    daprtest
    daprtest

--- amorphie.contract.zeebe/Modules/ZeebeContractInstance.cs
+++ amorphie.contract.zeebe/Modules/ZeebeContractInstance.cs
@@ -35,6 +35,14 @@ namespace amorphie.contract.zeebe.Modules
                           operation.Tags = new List<OpenApiTag> { new() { Name = nameof(ZeebeContractInstance) } };
                           return operation;
                       });
+                         app.MapPost("/contractinstancestate2", ContractInstanceState2)
+                      .Produces(StatusCodes.Status200OK)
+                      .WithOpenApi(operation =>
+                      {
+                          operation.Summary = "Maps ContractInstance service worker on Zeebe";
+                          operation.Tags = new List<OpenApiTag> { new() { Name = nameof(ZeebeContractInstance) } };
+                          return operation;
+                      });
             app.MapPost("/contractinstancestate", ContractInstanceState)
                                  .Produces(StatusCodes.Status200OK)
                                  .WithOpenApi(operation =>
@@ -91,6 +99,39 @@ namespace amorphie.contract.zeebe.Modules
 
         }
         static async ValueTask<IResult> ContractInstanceState(
+    [FromBody] dynamic body,
+   [FromServices] ProjectDbContext dbContext,
+    HttpRequest request,
+    HttpContext httpContext,
+    [FromServices] DaprClient client
+    , IConfiguration configuration,
+    [FromServices] IContractAppService contractAppService, CancellationToken token
+)
+        {
+            var messageVariables = ZeebeMessageHelper.VariablesControl(body);
+            var contractName = body.GetProperty("ContractInstanceState").GetProperty("contractName").ToString();
+            var reference = body.GetProperty("ContractInstanceState").GetProperty("reference").ToString();
+            var language = body.GetProperty("ContractInstanceState").GetProperty("language").ToString();
+            string bankEntity = body.GetProperty("ContractInstanceState").GetProperty("bankEntity").ToString();
+            var contract = new ContractInstanceInputDto
+            {
+                ContractName = contractName,
+            };
+            contract.SetHeaderParameters(new HeaderFilterModel
+            {
+                UserReference = reference,
+                LangCode = language,
+                EBankEntity = EBankEntity.on
+            });
+
+
+            var response = await contractAppService.InstanceState(contract, token);
+            messageVariables.Variables.Add("ContractInstanceStateResult", response);
+
+            messageVariables.Success = true;
+            return Results.Ok(ZeebeMessageHelper.CreateMessageVariables(messageVariables));
+        }
+         static async ValueTask<IResult> ContractInstanceState2(
     [FromBody] dynamic body,
    [FromServices] ProjectDbContext dbContext,
     HttpRequest request,
