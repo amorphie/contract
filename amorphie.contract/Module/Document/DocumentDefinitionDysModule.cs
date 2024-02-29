bf82446aa8564d6f3705efb13192f6a91f227966
Enes Can Işık <53035239+themiralay@users.noreply.github.com>
2024-02-29 15:04:23 +0300

    SOAP Requester Added for Dys
    SOAP Requester Added
    Dms Tag service Wrapping service added

--- amorphie.contract/Module/Document/DocumentDefinitionDysModule.cs
+++ amorphie.contract/Module/Document/DocumentDefinitionDysModule.cs
@@ -1,6 +1,8 @@
+using amorphie.contract.application;
 using amorphie.contract.core.Entity.Document;
 using amorphie.contract.data.Contexts;
 using amorphie.core.Module.minimal_api;
+using Microsoft.AspNetCore.Mvc;
 
 namespace amorphie.contract;
 
@@ -15,6 +17,24 @@ namespace amorphie.contract;
 
         public override string? UrlFragment => "document-definition-dys";
 
+        public override void AddRoutes(RouteGroupBuilder routeGroupBuilder)
+        {
+            base.AddRoutes(routeGroupBuilder);
+            routeGroupBuilder.MapGet("DmsTagList", TagList);
 
+        }
+
+        async ValueTask<IResult> TagList([FromServices] IDocumentDefinitionDysAppService documentDefinitionDysService, HttpContext httpContext,
+            CancellationToken token, [FromQuery] string referenceId)
+        {
+            var response = documentDefinitionDysService.GetAllTagsDys(referenceId,token);
+
+            return Results.Ok(new
+            {
+                Data = response,
+                Success = true,
+                ErrorMessage = "",
+            });
+        }
        
     }
