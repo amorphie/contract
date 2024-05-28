1dc51c0b4d2d12c2f59ba70f4ef419987d04b01b
Enes Can Işık <53035239+themiralay@users.noreply.github.com>
2024-05-28 09:06:53 +0300

    Lower version fix non contract doc
    Lower version fix non contract doc

--- amorphie.contract.application/Customer/Dto/CustomerDocumentsWithDefinitionDto.cs
+++ amorphie.contract.application/Customer/Dto/CustomerDocumentsWithDefinitionDto.cs
@@ -6,8 +6,8 @@ namespace amorphie.contract.application.Customer.Dto
 {
     public class CustomerDocumentsWithDefinitionDto
     {
-        public CustomerDocumentDto? CustomerDocumentDto { get; set; }
-        public CustomerDocumentDefinitionDto? CustomerDocumentDefinitionDto { get; set; }
+        public CustomerDocumentDto CustomerDocumentDto { get; set; }
+        public CustomerDocumentDefinitionDto CustomerDocumentDefinitionDto { get; set; }
     }
 
 }
