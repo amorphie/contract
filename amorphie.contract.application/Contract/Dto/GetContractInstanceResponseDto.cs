38577131e585ef2640bacee8b27a7c770eb35cc8
serhatdemir18 <sedemir@burgan.com.tr>
2024-05-29 11:56:49 +0300

    added Contract Title

--- amorphie.contract.application/Contract/Dto/GetContractInstanceResponseDto.cs
+++ amorphie.contract.application/Contract/Dto/GetContractInstanceResponseDto.cs
@@ -4,5 +4,7 @@ namespace amorphie.contract.application.Contract.Dto
     {
         public List<DocumentInstanceDto> DocumentList { get; set; } = new List<DocumentInstanceDto>();
         public List<DocumentGroupInstanceDto> DocumentGroupList { get; set; } = new List<DocumentGroupInstanceDto>();
+
+        public string ContractTitle { get; set; }
     }
 }
