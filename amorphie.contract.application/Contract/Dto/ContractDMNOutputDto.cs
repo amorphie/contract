8ff3d581ed00a93ad6d6f434598b79f3ff6be475
serhatdemir18 <sedemir@burgan.com.tr>
2024-06-07 14:35:03 +0300

    fixed dmn input

---
+++ amorphie.contract.application/Contract/Dto/ContractDMNOutputDto.cs
@@ -0,0 +1,8 @@
+namespace amorphie.contract.application.Contract.Dto
+{
+    public class ContractDMNOutputDto
+    {
+        public string DecisionTableId { get; set; }
+        public Dictionary<string, string> ContractDMNInput { get; set; }
+    }
+}
